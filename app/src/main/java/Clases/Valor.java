5842786c5f9909f461be87f2d3278ebf9ee86c3f
Mauricio Lopez <maurihlopez@gmail.com>
2020-10-08 01:09:36 -0300

    Primera versión terminada

---
+++ app/src/main/java/Clases/Valor.java
@@ -0,0 +1,26 @@
+package Clases;
+
+public class Valor {
+
+    private int farenheith;
+    private int revival;
+    private int elAlquimista;
+
+    public Valor(){
+        this.farenheith = 5000;
+        this.revival = 12000;
+        this.elAlquimista = 45000;
+    }
+
+    public int getFarenheith(){
+        return farenheith;
+    }
+
+    public int getRevival(){
+        return revival;
+    }
+
+    public int getElAlquimista(){
+        return elAlquimista;
+    }
+}
--- app/src/main/java/com/example/biblioteca/github_act.java
+++ app/src/main/java/com/example/biblioteca/github_act.java
@@ -3,12 +3,45 @@ package com.example.biblioteca;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import Clases.Valor;
 
 public class github_act extends AppCompatActivity {
+
+    private Spinner spinner;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_github_act);
+
+        spinner = (Spinner)findViewById(R.id.spinner);
+        tv = (TextView)findViewById(R.id.textView);
+
+        String [] libros = {"Farenheint", "Revival", "El Alquimista"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, libros);
+        spinner.setAdapter(adapter);
+    }
+
+    public void Mostrar(View view){
+        String seleccionLibro = spinner.getSelectedItem().toString();
+        Valor valor = new Valor();
+
+        if (seleccionLibro.equals("Farenheint")) {
+            tv.setText("El valor de " + seleccionLibro + " es: " + valor.getFarenheith());
+        }
+        if (seleccionLibro.equals("Revival")){
+            tv.setText("El valor de " + seleccionLibro + " es: " + valor.getRevival());
+        }
+        if(seleccionLibro.equals("El Alquimista")){
+            tv.setText("El valor de " + seleccionLibro + " es: " + valor.getElAlquimista());
+        }
     }
 }
