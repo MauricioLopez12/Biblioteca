329617af051b21842742497f133b131f4c603a71
Mauricio Lopez <maurihlopez@gmail.com>
2020-10-07 23:15:20 -0300

    Creado main, codificación y diseño

--- app/src/main/java/com/example/biblioteca/MainActivity.java
+++ app/src/main/java/com/example/biblioteca/MainActivity.java
@@ -2,13 +2,75 @@ package com.example.biblioteca;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar pb;
+    private Button button;
+
+    private ImageView iv_git;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pb = (ProgressBar)findViewById(R.id.progressBar);
+        button = (Button)findViewById(R.id.button);
+        iv_git = (ImageView)findViewById(R.id.imageView_git);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                new Task().execute();
+            }
+        });
+
+        pb.setVisibility(View.INVISIBLE);
+        iv_git.setVisibility(View.INVISIBLE);
+    }
+
+    class Task extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected void onPreExecute() {
+
+            pb.setVisibility(View.VISIBLE);
+
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            for(int i = 1; i < 10; i++)
+            {
+                try {
+                    Thread.sleep(1000);
+
+                }catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+        @Override // finalizamos la tarea
+        protected void onPostExecute(String s) {
+
+            pb.setVisibility(View.INVISIBLE);
+            iv_git.setVisibility(View.VISIBLE);
+        }
+
+        public void acceder(View view){
+            //Intent i = new Intent(this, );
+            //startActivity(i);
+        }
     }
 }
